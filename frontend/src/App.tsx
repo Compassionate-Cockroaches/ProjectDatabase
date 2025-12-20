097393bd63dd3428779a1d8e147c518cd1a3c0d1
TheDLCrimson <duclnc2004@gmail.com>
2025-12-21 02:39:42 +0700

    [FEAT] Refactor App and main components to implement routing and query client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
-import { ComponentExample } from "@/components/component-example";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import UsersPage from "./pages/UsersPage";
 
 export function App() {
-return <ComponentExample />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="users" element={<UsersPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
